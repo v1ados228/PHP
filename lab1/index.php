c9e806fb3b041a9ce7dddbf05213c7f6322207a3
v1ados228 <vladmorozov06@gmail.com>
2025-04-12 12:52:34 +0300

    lab1

---
+++ lab1/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Морозов Владислав Алексеевич 241-321</title>
+</head>
+<body>
+    <header style="display: flex; justify-content: left; gap: 5%;">
+        <img src="./Logo_Polytech_rus_main.jpg" alt="Логотип" style="width: 30%; height: 20%;">
+        <h1>Домашняя работа: Hello, World!</h1>
+    </header>
+    <main>
+        <p>
+            <?php echo "Hello, world!" ?>
+        </p>
+        <p>
+            <?php echo date("l");?>
+        </p>
+    </main>
+    <footer>
+        <p>Задание для самостоятельной работы</p>
+    </footer>
+</body>
+</html>
