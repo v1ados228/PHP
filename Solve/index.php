45b2640bfbea6faa97669fa2b79c462b45f9b474
v1ados228 <vladmorozov06@gmail.com>
2025-04-12 14:26:55 +0300

    sdelal

---
+++ Solve/index.php
@@ -0,0 +1,36 @@
+<?php
+$equation = "X + 67 = 129";
+
+$equation = str_replace(' ', '', $equation);
+
+[$left, $right] = explode('=', $equation);
+
+if (strpos($left, 'X') !== false) {
+    $expr = $left;
+    $result = (int)$right;
+} 
+else {
+    $expr = $right;
+    $result = (int)$left;
+}
+
+if (preg_match('/X([\+\-\*\/])(\d+)/', $expr, $matches)) {
+    $op = $matches[1];
+    $num = (int)$matches[2];
+
+    switch ($op) {
+        case '+': $x = $result - $num; break;
+        case '-': $x = $result + $num; break;
+        case '*': $x = $result / $num; break;
+        case '/': $x = $result * $num; break;
+    }
+
+    echo "Уравнение: $equation<br>";
+    echo "Оператор: $op<br>";
+    echo "Число: $num<br>";
+    echo "Значение X: $x";
+} 
+else {
+    echo "Формат уравнения не распознан.";
+}
+?>
